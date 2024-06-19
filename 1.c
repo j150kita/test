5ef696b6f4f32430e3deeb8a9bcd216f02267b81
j150kita <j150kita@sshg.cs.ehime-u.ac.jp>
2024-06-19 13:26:55 +0900

    Update 1.c with new changes

--- 1.c
+++ 1.c
@@ -5,9 +5,13 @@ int main(void)
   int n;
 
   printf("n = ");
-  scanf("%d", &n);
 
-  if( n & 1 == 0 ){
+   if (scanf("%d", &n) != 1) {
+    printf("入力エラーが発生しました。\n");
+    return 1; // エラーコードを返してプログラムを終了する
+  }
+  
+  if( (n & 1) == 0 ){
     printf("%d は偶数です\n", n);
   }else{
     printf("%d は奇数です\n", n);
